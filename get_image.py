67f296d7dabdb76de84480955237a5b2bf90e485
singpou <singpoulee@gmail.com>
2018-11-30 09:40:16 -0500

    modify code to allow us to specify the layers for the loss

--- get_image.py
+++ get_image.py
@@ -4,103 +4,59 @@ from PIL import Image
 import vgg19_truncate
 
 # define loss function for content
-def get_content_loss(vgg_image, vgg_content, layer="conv4_2"):
+def get_content_loss(vgg_image, vgg_content, layer=4):
 
-    if layer == "conv4_2":
-        content_loss = tf.nn.l2_loss(vgg_content.conv4_2 - vgg_image.conv4_2)
+    content_op_list = [vgg_content.conv1_2, vgg_content.conv2_2, vgg_content.conv3_2,
+                       vgg_content.conv4_2, vgg_content.conv5_2]
 
-    # print("content_loss=", content_loss)
+    img_op_list = [vgg_image.conv1_2, vgg_image.conv2_2, vgg_image.conv3_2,
+                   vgg_image.conv4_2, vgg_image.conv5_2]
+
+    content_loss = tf.nn.l2_loss(content_op_list[layer-1] - img_op_list[layer-1])
 
     return content_loss
 
-# define loss function for style
-def get_style_loss(vgg_image, vgg_style, layer="conv5_2"):
 
-    if layer == "conv5_2":
+# define loss function for style
+def get_style_loss(vgg_image, vgg_style, layers=5):
+
+    style_op_list = [vgg_style.conv1_2, vgg_style.conv2_2, vgg_style.conv3_2, vgg_style.conv4_2, vgg_style.conv5_2]
+    img_op_list = [vgg_image.conv1_2, vgg_image.conv2_2, vgg_image.conv3_2, vgg_image.conv4_2, vgg_image.conv5_2]
+
+    total_style_loss = 0
+
+    for layer_op_num in range(layers):
 
         # since there is only one style image, there is only one output; get the first output
-        conv5_2_style_op = vgg_style.conv5_2[0]
-        #flatten the w and h dim of output of conv5_2; retain channel as a separate dim
-        conv5_2_style_op = tf.reshape(conv5_2_style_op, shape=(-1, vgg_style.conv5_2.shape[3]))
+        cur_style_op = style_op_list[layer_op_num][0]
+
+        #flatten the w and h dim of output of current layer; retain channel as a separate dim
+        cur_style_op = tf.reshape(cur_style_op, shape=(-1, style_op_list[layer_op_num][3]))
+
         # do the same for output for desired image
-        conv5_2_img_op = vgg_image.conv5_2[0]
-        conv5_2_img_op = tf.reshape(conv5_2_img_op, shape=(-1, vgg_image.conv5_2.shape[3]))
+        cur_img_op = img_op_list[layer_op_num][0]
+        cur_img_op = tf.reshape(cur_img_op, shape=(-1, img_op_list[layer_op_num][3]))
 
-        #print("conv5_2_style_op.shape=", conv5_2_style_op.shape)
+        # get style loss from current layer
+        cur_style_loss = tf.nn.l2_loss(tf.matmul(tf.transpose(cur_style_op), cur_style_op) -
+                                    tf.matmul(tf.transpose(cur_img_op), cur_img_op))
 
-        # get style loss from conv5_2
-        style_loss5 = tf.nn.l2_loss(tf.matmul(tf.transpose(conv5_2_style_op), conv5_2_style_op) -
-                                    tf.matmul(tf.transpose(conv5_2_img_op), conv5_2_img_op))
+        cur_style_loss /= tf.to_float(tf.multiply(tf.square(tf.shape(style_op_list[layer_op_num])[1]),
+                                  tf.square(tf.shape(style_op_list[layer_op_num])[3]))) * 2
 
-        #print("style_loss.dtype", style_loss.dtype)
-
-        # get style loss from conv5_2
-        style_loss5 /= tf.to_float(tf.multiply(tf.square(tf.shape(vgg_style.conv5_2)[1]),
-                                  tf.square(tf.shape(vgg_style.conv5_2)[3]))) * 2
+        total_style_loss += cur_style_loss
 
 
-        # do the same for conv4_2 like we do for conv5_2
-        conv4_2_style_op = vgg_style.conv4_2[0]
-        conv4_2_style_op = tf.reshape(conv4_2_style_op, shape=(-1, vgg_style.conv4_2.shape[3]))
-        conv4_2_img_op = vgg_image.conv4_2[0]
-        conv4_2_img_op = tf.reshape(conv4_2_img_op, shape=(-1, vgg_image.conv4_2.shape[3]))
+    # scale the loss by the number of layers
+    total_style_loss /= layers
 
-        style_loss4 = tf.nn.l2_loss(tf.matmul(tf.transpose(conv4_2_style_op), conv4_2_style_op) -
-                                    tf.matmul(tf.transpose(conv4_2_img_op), conv4_2_img_op))
-
-        style_loss4 /= tf.to_float(tf.multiply(tf.square(tf.shape(vgg_style.conv4_2)[1]),
-                                  tf.square(tf.shape(vgg_style.conv4_2)[3]))) * 2
-
-        conv3_2_style_op = vgg_style.conv3_2[0]
-        conv3_2_style_op = tf.reshape(conv3_2_style_op, shape=(-1, vgg_style.conv3_2.shape[3]))
-        conv3_2_img_op = vgg_image.conv3_2[0]
-        conv3_2_img_op = tf.reshape(conv3_2_img_op, shape=(-1, vgg_image.conv3_2.shape[3]))
-
-        style_loss3 = tf.nn.l2_loss(tf.matmul(tf.transpose(conv3_2_style_op), conv3_2_style_op) -
-                                    tf.matmul(tf.transpose(conv3_2_img_op), conv3_2_img_op))
-
-        style_loss3 /= tf.to_float(tf.multiply(tf.square(tf.shape(vgg_style.conv3_2)[1]),
-                                  tf.square(tf.shape(vgg_style.conv3_2)[3]))) * 2
-
-        conv2_2_style_op = vgg_style.conv2_2[0]
-        conv2_2_style_op = tf.reshape(conv2_2_style_op, shape=(-1, vgg_style.conv2_2.shape[3]))
-        conv2_2_img_op = vgg_image.conv2_2[0]
-        conv2_2_img_op = tf.reshape(conv2_2_img_op, shape=(-1, vgg_image.conv2_2.shape[3]))
-
-        style_loss2 = tf.nn.l2_loss(tf.matmul(tf.transpose(conv2_2_style_op), conv2_2_style_op) -
-                                    tf.matmul(tf.transpose(conv2_2_img_op), conv2_2_img_op))
-
-        style_loss2 /= tf.to_float(tf.multiply(tf.square(tf.shape(vgg_style.conv2_2)[1]),
-                                  tf.square(tf.shape(vgg_style.conv2_2)[3]))) * 2
-
-        conv1_2_style_op = vgg_style.conv1_2[0]
-        conv1_2_style_op = tf.reshape(conv1_2_style_op, shape=(-1, vgg_style.conv1_2.shape[3]))
-        conv1_2_img_op = vgg_image.conv1_2[0]
-        conv1_2_img_op = tf.reshape(conv1_2_img_op, shape=(-1, vgg_image.conv1_2.shape[3]))
-
-        style_loss1 = tf.nn.l2_loss(tf.matmul(tf.transpose(conv1_2_style_op), conv1_2_style_op) -
-                                    tf.matmul(tf.transpose(conv1_2_img_op), conv1_2_img_op))
-
-        style_loss1 /= tf.to_float(tf.multiply(tf.square(tf.shape(vgg_style.conv1_2)[1]),
-                                  tf.square(tf.shape(vgg_style.conv1_2)[3]))) *  2
-
-
-        style_loss = style_loss5 + style_loss4 + style_loss3 + style_loss2 + style_loss1
-
-        style_loss *= 0.2
-
-    # print("style_loss=", style_loss)
-
-    return style_loss
+    return total_style_loss
 
 
 # define total loss function
-
 def get_total_loss(alpha, beta, content_loss, style_loss):
 
     total_loss = alpha * content_loss + beta * style_loss
-
-    # print("total_loss=", total_loss)
 
     return total_loss
 
@@ -117,6 +73,13 @@ def train_step(loss, learning_rate=5e-3, optimizer="adam"):
 
     return step
 
+def normalize_img(img_array):
+    # normalize image to range of [0, 1]
+    img_array = (img_array - np.min(img_array)) / (np.max(img_array) - np.min(img_array))
+    img_array = img_array.reshape((1,) + img_array.shape)
+
+    return img_array
+
 
 def get_image(image_path, content_image, style_image, vgg_params_path, iterations=10, alpha_beta_ratio=0.001):
 
@@ -124,12 +87,8 @@ def get_image(image_path, content_image, style_image, vgg_params_path, iteration
     style_image1  = np.array(Image.open(image_path + style_image), dtype='float32')
 
     # normalize image to range of [0, 1]
-    content_image1 = (content_image1 - np.min(content_image1)) / (np.max(content_image1) - np.min(content_image1))
-    style_image1 = (style_image1 - np.min(style_image1)) / (np.max(style_image1) - np.min(style_image1))
-
-    # normalize image to range of [0, 1]
-    content_image1 = content_image1.reshape((1,) + content_image1.shape)
-    style_image1 = style_image1.reshape((1,) + style_image1.shape)
+    content_image1 = normalize_img(content_image1)
+    style_image1 = normalize_img(style_image1)
 
     # construct noisy image
     image_shape = content_image1.shape
